3a521f3ff5a9f6675169c1a829f1ea6acad684e7
ohsung0722 <99doldol@gmail.com>
2024-08-03 17:25:45 +0900

    [ADD] 백준 3273번

--- array/B3273.cpp
+++ array/B3273.cpp
@@ -1,6 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int a[100001];
+int b[2000001] = {};
+
 int main(void){
-    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    int x;
+    cin >> x;
+
+    int count = 0;
+
+    for(int i = 0; i < n; i++){
+        if(x-a[i] > 0){
+            if(b[x - a[i]] == 1){
+                count++;
+            }
+        }
+        
+        b[a[i]] = 1;
+    }
+
+    cout << count;
 }
