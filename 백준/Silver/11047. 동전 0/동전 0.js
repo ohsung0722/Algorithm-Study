a2538ea1031010fab1b8bbb14cad10ce62caef11
ohsung0722 <107835039+ohsung0722@users.noreply.github.com>
2025-09-18 16:14:19 +0900

    [Silver IV] Title: 동전 0, Time: 216 ms, Memory: 9632 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const filePath =
+  process.platform === "linux" ? "dev/stdin" : "Baekjoon/B11047/input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const coin = input[0].split(" ").map(Number);
+const n = coin[0];
+const k = coin[1];
+
+const array = new Array();
+
+for (let i = 1; i < n + 1; i++) {
+  array.push(Number(input[i]));
+}
+
+let restCoin = k;
+let count = 0;
+for (let i = n; i >= 0; i--) {
+  if (restCoin === 0) {
+    break;
+  }
+  if (array[i] > k) {
+    continue;
+  }
+
+  while (array[i] <= restCoin) {
+    restCoin -= array[i];
+    count++;
+  }
+}
+
+console.log(count);
