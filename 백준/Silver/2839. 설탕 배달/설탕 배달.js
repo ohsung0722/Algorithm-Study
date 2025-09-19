42ebfec265c9609fc7ea3c97362d7467970f7192
ohsung0722 <107835039+ohsung0722@users.noreply.github.com>
2025-09-19 18:12:58 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -3,44 +3,18 @@ const filePath =
   process.platform === "linux" ? "dev/stdin" : "Baekjoon/B2839/input.txt";
 const input = fs.readFileSync(filePath).toString().trim();
 
-const n = Number(input);
+let n = Number(input);
 
-//5로 나눈 나머지 저장
-let a = 0;
+let res = 0;
 
-//5키로 개수
-let count_5 = 0;
-let count_3 = 0;
-
-count_5 = Math.floor(n / 5);
-a = n % 5;
-if (a == 0) {
-  console.log(count_5);
-} else if (a == 1) {
-  if (count_5 >= 1) {
-    count_5 -= 1;
-    count_3 = 2;
-    console.log(count_5 + count_3);
+while (n >= 3) {
+  if (n % 5 === 0) {
+    n -= 5;
+    res++;
   } else {
-    console.log(-1);
-  }
-} else if (a == 2) {
-  if (count_5 >= 2) {
-    count_5 -= 2;
-    count_3 = 4;
-    console.log(count_5 + count_3);
-  } else {
-    console.log(-1);
-  }
-} else if (a == 3) {
-  count_3 = 1;
-  console.log(count_5 + count_3);
-} else {
-  if (count_5 >= 1) {
-    count_5 -= 1;
-    count_3 = 3;
-    console.log(count_5 + count_3);
-  } else {
-    console.log(-1);
+    n -= 3;
+    res++;
   }
 }
+
+n === 0 ? console.log(res) : console.log(-1);
