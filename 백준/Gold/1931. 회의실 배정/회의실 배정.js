7a4b00d899b43204d4069ea74aeeffe1518fc8e2
ohsung0722 <107835039+ohsung0722@users.noreply.github.com>
2025-09-22 11:56:50 +0900

    [Gold V] Title: 회의실 배정, Time: 360 ms, Memory: 45568 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const filePath =
+  process.platform === "linux" ? "dev/stdin" : "Baekjoon/B1931/input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let n = Number(input[0]);
+let array = new Array();
+for (let i = 1; i <= n; i++) {
+  array.push(input[i].split(" ").map(Number));
+}
+
+let minValue = BigInt(0);
+let count = 0;
+
+array.sort((a, b) => {
+  if (a[1] !== b[1]) return a[1] - b[1];
+  else return a[0] - b[0];
+});
+
+for (let i = 0; i < n; i++) {
+  if (minValue <= BigInt(array[i][0])) {
+    minValue = BigInt(array[i][1]);
+    count++;
+  }
+}
+
+console.log(count);
