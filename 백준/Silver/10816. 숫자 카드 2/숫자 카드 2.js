686354f96873091f7d16f48cc459d0248528bcc6
ohsung0722 <107835039+ohsung0722@users.noreply.github.com>
2025-10-14 11:53:58 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 772 ms, Memory: 166024 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -3,44 +3,17 @@ const filePath =
   process.platform === "linux" ? "dev/stdin" : "Baekjoon/B10816/input.txt";
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-function lowerBound(arr, target) {
-  let start = 0;
-  let end = arr.length;
-  while (start < end) {
-    let mid = Math.floor((start + end) / 2);
-    if (arr[mid] >= target) {
-      end = mid;
-    } else {
-      start = mid + 1;
-    }
-  }
-  return end;
-}
-
-function upperBound(arr, target) {
-  let start = 0;
-  let end = arr.length;
-  while (start < end) {
-    let mid = Math.floor((start + end) / 2);
-    if (arr[mid] > target) {
-      end = mid;
-    } else {
-      start = mid + 1;
-    }
-  }
-  return end;
-}
-
-let n = Number(input[0]);
-let m = Number(input[2]);
 let array1 = input[1].split(" ").map(Number);
 let array2 = input[3].split(" ").map(Number);
 
-array1.sort((a, b) => a - b);
+const map = new Map();
+for (x of array1) {
+  map.set(x, (map.get(x) || 0) + 1);
+}
 
 let result = "";
-for (i of array2) {
-  result += upperBound(array1, i) - lowerBound(array1, i) + " ";
+for (x of array2) {
+  result += (map.get(x) || 0) + " ";
 }
 
 console.log(result);
