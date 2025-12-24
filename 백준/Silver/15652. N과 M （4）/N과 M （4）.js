a6515c3e0c8e868717548d2b88359595e9cc2e77
ohsung0722 <107835039+ohsung0722@users.noreply.github.com>
2025-12-24 17:36:35 +0900

    [Silver III] Title: N과 M (4), Time: 172 ms, Memory: 16628 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const filePath =
+  process.platform === "linux" ? "dev/stdin" : "Baekjoon/B15652/input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split(" ");
+
+const N = Number(input[0]);
+const M = Number(input[1]);
+
+let arr = [];
+for (let i = 1; i <= N; i++) {
+  arr.push(i);
+}
+
+let select = [];
+
+let answer = "";
+function dfs(arr, depth) {
+  if (depth === M) {
+    let result = [];
+    for (let i of select) {
+      result.push(arr[i]);
+    }
+    for (let i of result) {
+      answer += i + " ";
+    }
+    answer += "\n";
+    return;
+  }
+
+  for (let i = 0; i < N; i++) {
+    if (select.length > 0 && select[select.length - 1] > i) {
+      continue;
+    }
+    select.push(i);
+    dfs(arr, depth + 1);
+    select.pop();
+  }
+}
+
+dfs(arr, 0);
+console.log(answer);
