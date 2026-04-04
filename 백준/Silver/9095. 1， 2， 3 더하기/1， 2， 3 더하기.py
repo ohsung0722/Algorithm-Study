584ed2e9995c1be00186a9dbf3798455520e9514
ohsung0722 <107835039+ohsung0722@users.noreply.github.com>
2026-04-04 13:05:50 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,29 @@
+t = int(input())
+
+def dpf(n):
+    dp = [0] * (n + 1)
+    dp[1] = 1 
+    dp[2] = 2
+    dp[3] = 4
+
+    for i in range(4, n + 1):
+        dp[i] = dp[i-1] + dp[i - 2] + dp[i - 3]
+    
+    return dp[n]
+
+for i in range(t):
+    n = int(input())
+
+    if n == 1:
+        print(1)
+        continue
+    if n == 2:
+        print(2)
+        continue
+    if n == 3:
+        print(4)
+        continue
+
+    count = dpf(n)
+    print(count)
+
