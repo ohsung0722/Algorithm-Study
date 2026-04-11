d7f4b0cafbeb3c533302ae932cce06156c00e889
ohsung0722 <107835039+ohsung0722@users.noreply.github.com>
2026-04-11 13:11:50 +0900

    [Gold V] Title: 평범한 배낭, Time: 2480 ms, Memory: 36264 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [0] * (k + 1)
+
+for w, v in arr:
+    for j in range(k, w - 1, -1):
+        dp[j] = max(dp[j], dp[j - w] + v)
+
+print(dp[k])
